bffd0dad16f7c8b3f1923bf0d506fa3bc03a4feb
ammmmmmmmyy <angela_mae_yambao@dlsl.edu.ph>
2022-11-17 01:16:02 +0800

    Add project files.

---
+++ ThesisCNN/SignUP_Page.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ThesisCNN
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignUP_Page : ContentPage
+    {
+        public SignUP_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
