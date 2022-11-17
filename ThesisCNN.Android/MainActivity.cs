3edb03337440801471a2c3d487f51218a9acb7ef
サイカ <haroldcabungcalcc@gmail.com>
2022-11-17 16:01:58 +0800

    Added Speech to Text scripts

--- ThesisCNN.Android/MainActivity.cs
+++ ThesisCNN.Android/MainActivity.cs
@@ -1,20 +1,25 @@
 ﻿using System;
 
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android.Speech;
+using Xamarin.Forms;
 
 namespace ThesisCNN.Droid
 {
-    [Activity(Label = "ThesisCNN", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    [Activity(Label = "ThesisCNN", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity, IMessageSender
     {
+        private readonly int VOICE = 10;
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(savedInstanceState);
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
@@ -23,6 +28,27 @@ namespace ThesisCNN.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            if (requestCode == VOICE)
+            {
+                if (resultCode == Result.Ok)
+                {
+                    var matches = data.GetStringArrayListExtra(RecognizerIntent.ExtraResults);
+                    if (matches.Count != 0)
+                    {
+                        string textInput = matches[0];
+                        MessagingCenter.Send<IMessageSender, string>(this, "STT", textInput);
+                    }
+                    else
+                    {
+                        MessagingCenter.Send<IMessageSender, string>(this, "STT", "No input");
+                    }
+
+                }
+            }
+            base.OnActivityResult(requestCode, resultCode, data);
         }
     }
 }
--- ThesisCNN/MainMenu_SpeechTextTrans.xaml.cs
+++ ThesisCNN/MainMenu_SpeechTextTrans.xaml.cs
@@ -6,15 +6,62 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using static Xamarin.Forms.Internals.GIFBitmap;
 
 namespace ThesisCNN
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainMenu_SpeechTextTrans : ContentPage
     {
+        private ISpeechToText _speechRecongnitionInstance;
         public MainMenu_SpeechTextTrans()
         {
             InitializeComponent();
+            try
+            {
+                _speechRecongnitionInstance = DependencyService.Get<ISpeechToText>();
+            }
+            catch (Exception ex)
+            {
+                recon.Text = ex.Message;
+            }
+
+
+            MessagingCenter.Subscribe<ISpeechToText, string>(this, "STT", (sender, args) =>
+            {
+                SpeechToTextFinalResultRecieved(args);
+            });
+
+            MessagingCenter.Subscribe<ISpeechToText>(this, "Final", (sender) =>
+            {
+                start.IsEnabled = true;
+            });
+
+            MessagingCenter.Subscribe<IMessageSender, string>(this, "STT", (sender, args) =>
+            {
+                SpeechToTextFinalResultRecieved(args);
+            });
+        }
+
+        private void SpeechToTextFinalResultRecieved(string args)
+        {
+            recon.Text = args;
+        }
+
+        private void Start_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                _speechRecongnitionInstance.StartSpeechToText();
+            }
+            catch (Exception ex)
+            {
+                recon.Text = ex.Message;
+            }
+
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                start.IsEnabled = false;
+            }
         }
     }
 }
