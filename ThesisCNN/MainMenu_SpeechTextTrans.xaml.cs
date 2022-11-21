d2c082348d43162afe07b6dd9ee2b1920a71dc2d
ammmmmmmmyy <angela_mae_yambao@dlsl.edu.ph>
2022-11-21 23:17:32 +0800

    update on speech to text page - updated ui -removed back button - added text to speech icon - added main menu button

--- ThesisCNN/MainMenu_SpeechTextTrans.xaml.cs
+++ ThesisCNN/MainMenu_SpeechTextTrans.xaml.cs
@@ -63,7 +63,61 @@ namespace ThesisCNN
                 start.IsEnabled = false;
             }
         }
+        //MAIN BUTTON
+        async void Button_mainmenu(object sender, EventArgs e)
+        {
+            if (button_noiseReduction.IsVisible == false && button_cam.IsVisible == false && button_speech.IsVisible == false)
+            {
+                button_noiseReduction.IsVisible = true;
+                button_cam.IsVisible = true;
+                button_speech.IsVisible = true;
 
-        
+                await Task.WhenAll(
+                    button_noiseReduction.TranslateTo(0, -90, 500),
+                    button_cam.TranslateTo(130, 0, 500),
+                    button_speech.TranslateTo(-130, 0, 500)
+                    );
+
+                button_noiseReduction.IsEnabled = true;
+                button_cam.IsEnabled = true;
+                button_speech.IsEnabled = true;
+            }
+            else
+            {
+                button_noiseReduction.IsEnabled = false;
+                button_cam.IsEnabled = false;
+                button_speech.IsEnabled = false;
+
+                await Task.WhenAll(
+                    button_noiseReduction.TranslateTo(0, 50, 500),
+                    button_cam.TranslateTo(-20, 0, 500),
+                    button_speech.TranslateTo(20, 0, 500)
+                    );
+
+                button_noiseReduction.IsVisible = false;
+                button_cam.IsVisible = false;
+                button_speech.IsVisible = false;
+            }
+        }
+        //MAIN MENU NOISE REDUCTION 
+        private async void Clicked_button_noiseReduction(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"//{nameof(MainMenu_NoiseReduct)}");
+        }
+        //MAIN MENU CAM
+        private async void Clicked_button_cam(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"//{nameof(MainMenu_SignLangCapture)}");
+        }
+        //MAIN MENU SPEECH TO TEXT
+        private async void Clicked_button_speech(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"//{nameof(MainMenu_TextToSpeech)}");
+        }
+        private async void Button_text_speech(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"//{nameof(MainMenu_TextToSpeech)}");
+        }
+
     }
 }
