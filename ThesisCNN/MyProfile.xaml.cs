7628da335374cb6e993b92310f72c40ec7f2fbcc
ammmmmmmmyy <angela_mae_yambao@dlsl.edu.ph>
2022-12-27 23:31:22 +0800

    update: myprofile

--- ThesisCNN/MyProfile.xaml.cs
+++ ThesisCNN/MyProfile.xaml.cs
@@ -22,7 +22,7 @@ namespace ThesisCNN
         }
         //Back button
         public ICommand GoBackCommand => new Command(async () => await Shell.Current.GoToAsync($"//{nameof(MainMenu_NoiseReduct)}"));
-        //
+        //UPDATES NAME AND EMAIL
         private async void Button_SaveChanges(object sender, EventArgs e)
         {
             string dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "contactData.db");
@@ -37,10 +37,52 @@ namespace ThesisCNN
 
             if (pincodequery != null)
             {
-                pincodequery.Name = change_name.Text;
-                db.Update(pincodequery);
-                await DisplayAlert("Notification", "df.", "OK");
-                nameaaa.Text = pincodequery.Name;
+                if (string.IsNullOrEmpty(change_name.Text) && string.IsNullOrEmpty(change_email.Text))
+                {
+                    await DisplayAlert("Notification", "Please input to update. Thank you!", "OK");
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(change_email.Text).Equals(false) && string.IsNullOrEmpty(change_name.Text).Equals(false))
+                    {
+                        pincodequery.Name = change_name.Text;
+                        pincodequery.Email = change_email.Text;
+                        db.Update(pincodequery);
+
+                        await DisplayAlert("Notification", "Email and Name Updated.", "OK");
+                        nameaaa.Text = pincodequery.Name;
+                        emaiilll.Text = pincodequery.Email;
+
+                        change_name.Text = null;
+                        change_email.Text = null;
+
+                    }
+                    else if (string.IsNullOrEmpty(change_name.Text) && string.IsNullOrEmpty(change_email.Text).Equals(false))
+                    {
+                            pincodequery.Email = change_email.Text;
+                            db.Update(pincodequery);
+
+                            await DisplayAlert("Notification", "Email updated.", "OK");
+                            emaiilll.Text = pincodequery.Email;
+
+                            change_name.Text = null;
+                            change_email.Text = null;
+
+                    }
+                    else if (string.IsNullOrEmpty(change_email.Text) && string.IsNullOrEmpty(change_name.Text).Equals(false))
+                    {
+                        pincodequery.Name = change_name.Text;
+                        db.Update(pincodequery);
+
+                        await DisplayAlert("Notification", "Name Updated.", "OK");
+                        nameaaa.Text = pincodequery.Name;
+
+                        change_name.Text = null;
+                        change_email.Text = null;
+                    }
+                    await Shell.Current.GoToAsync($"//{nameof(MainMenu_NoiseReduct)}");
+                }
+               
             }
 
         }
