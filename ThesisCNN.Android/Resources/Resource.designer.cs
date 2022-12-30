eb01a03c5c7246f17e08856ac040012c703decea
サイカ <haroldcabungcalcc@gmail.com>
2022-12-30 20:36:18 +0800

    Prepared application for building

--- ThesisCNN.Android/Resources/Resource.designer.cs
+++ ThesisCNN.Android/Resources/Resource.designer.cs
@@ -22269,22 +22269,25 @@ namespace ThesisCNN.Droid
 			public const int preference_list_divider_material = 2131165338;
 			
 			// aapt resource value: 0x7F07009B
-			public const int startPageBG = 2131165339;
+			public const int signit_icon = 2131165339;
 			
 			// aapt resource value: 0x7F07009C
-			public const int test_custom_background = 2131165340;
+			public const int startPageBG = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int text_speech = 2131165341;
+			public const int test_custom_background = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int tooltip_frame_dark = 2131165342;
+			public const int text_speech = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int tooltip_frame_light = 2131165343;
+			public const int tooltip_frame_dark = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int verified = 2131165344;
+			public const int tooltip_frame_light = 2131165344;
+			
+			// aapt resource value: 0x7F0700A1
+			public const int verified = 2131165345;
 			
 			static Drawable()
 			{
