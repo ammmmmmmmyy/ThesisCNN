8b0294a84b8a13aa256ee163f8609cb548f243fc
ammmmmmmmyy <angela_mae_yambao@dlsl.edu.ph>
2023-01-23 09:53:46 +0800

    update: local html to xamarin

---
+++ ThesisCNN.Android/BaseUrl_Android.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ThesisCNN.Droid;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(BaseUrl_Android))]
+namespace ThesisCNN.Droid
+{
+	public class BaseUrl_Android : IBaseUrl
+	{
+		public string Get()
+		{
+			return "file:///android_asset/";
+		}
+	}
+}
--- ThesisCNN/MainMenu_SignLangCapture.xaml.cs
+++ ThesisCNN/MainMenu_SignLangCapture.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Xamarin.CommunityToolkit.UI.Views;
 using Xamarin.Forms;
@@ -7,108 +8,24 @@ using Xamarin.Forms.Xaml;
 
 namespace ThesisCNN
 {
+    public interface IBaseUrl { string Get(); }
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainMenu_SignLangCapture : ContentPage
     {
         public MainMenu_SignLangCapture()
         {
             InitializeComponent();
-            xctCameraView.Shutter();
+
+            var urlSource = new UrlWebViewSource();
+
+            string baseUrl = DependencyService.Get<IBaseUrl>().Get();
+            string filePathUrl = Path.Combine(baseUrl, "index.html");
+            urlSource.Url = filePathUrl;
+            webviewjava.Source = urlSource;
+
+
         }
-        async void Button_mainmenu(object sender, EventArgs e)
-        {
-            if (button_noiseReduction.IsVisible == false && button_cam.IsVisible == false && button_speech.IsVisible == false)
-            {
-                button_noiseReduction.IsVisible = true;
-                button_cam.IsVisible = true;
-                button_speech.IsVisible = true;
-
-                await Task.WhenAll(
-                    button_noiseReduction.TranslateTo(0, -90, 300),
-                    button_cam.TranslateTo(130, 0, 300),
-                    button_speech.TranslateTo(-130, 0, 300)
-                    );
-
-                button_noiseReduction.IsEnabled = true;
-                button_cam.IsEnabled = true;
-                button_speech.IsEnabled = true;
-            }
-            else
-            {
-                button_noiseReduction.IsEnabled = false;
-                button_cam.IsEnabled = false;
-                button_speech.IsEnabled = false;
-
-                await Task.WhenAll(
-                    button_noiseReduction.TranslateTo(0, 50, 300),
-                    button_cam.TranslateTo(-20, 0, 300),
-                    button_speech.TranslateTo(20, 0, 300)
-                    );
-
-                button_noiseReduction.IsVisible = false;
-                button_cam.IsVisible = false;
-                button_speech.IsVisible = false;
-            }
-        }
-
-        //MAIN MENU NOISE REDUCTION 
-        private async void Clicked_button_noiseReduction(object sender, EventArgs e)
-        {
-            button_noiseReduction.IsEnabled = false;
-            button_cam.IsEnabled = false;
-            button_speech.IsEnabled = false;
-
-            await Task.WhenAll(
-                button_noiseReduction.TranslateTo(0, 50, 300),
-                button_cam.TranslateTo(-20, 0, 300),
-                button_speech.TranslateTo(20, 0, 300)
-                );
-
-            button_noiseReduction.IsVisible = false;
-            button_cam.IsVisible = false;
-            button_speech.IsVisible = false;
-
-            await Shell.Current.GoToAsync($"//{nameof(MainMenu_NoiseReduct)}");
-        }
-        //MAIN MENU CAM
-        private async void Clicked_button_cam(object sender, EventArgs e)
-        {
-            button_noiseReduction.IsEnabled = false;
-            button_cam.IsEnabled = false;
-            button_speech.IsEnabled = false;
-
-            await Task.WhenAll(
-                button_noiseReduction.TranslateTo(0, 50, 300),
-                button_cam.TranslateTo(-20, 0, 300),
-                button_speech.TranslateTo(20, 0, 300)
-                );
-
-            button_noiseReduction.IsVisible = false;
-            button_cam.IsVisible = false;
-            button_speech.IsVisible = false;
-
-            await Shell.Current.GoToAsync($"//{nameof(MainMenu_SignLangCapture)}");
-        }
-        //MAIN MENU SPEECH TO TEXT
-        private async void Clicked_button_speech(object sender, EventArgs e)
-        {
-            button_noiseReduction.IsEnabled = false;
-            button_cam.IsEnabled = false;
-            button_speech.IsEnabled = false;
-
-            await Task.WhenAll(
-                button_noiseReduction.TranslateTo(0, 50, 300),
-                button_cam.TranslateTo(-20, 0, 300),
-                button_speech.TranslateTo(20, 0, 300)
-                );
-
-            button_noiseReduction.IsVisible = false;
-            button_cam.IsVisible = false;
-            button_speech.IsVisible = false;
-
-            await Shell.Current.GoToAsync($"//{nameof(MainMenu_TextToSpeech)}");
-        }
-        //CAMERA 
+       
         
         
     }
